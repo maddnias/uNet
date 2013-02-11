469d4323e62eaa0dff44be96e5d09079e862a5b6
UbbeLoL <netdeob@gmail.com>
2013-02-11 21:43:04 +0100

    Credits

--- uNet/Tools/Extensions/BinaryStreamExtensions.cs
+++ uNet/Tools/Extensions/BinaryStreamExtensions.cs
@@ -10,6 +10,7 @@ namespace uNet.Tools.Extensions
 {
     public static class BinaryStreamExtensions
     {
+        //Thanks 0xDEADDEAD
         public static void WriteEncodedInteger(this BinaryWriter bw, ulong integer)
         {
             do
@@ -21,6 +22,7 @@ namespace uNet.Tools.Extensions
             } while (integer != 0);
         }
 
+        //Thanks 0xDEADDEAD
         public static ulong ReadEncodedInteger(this BinaryReader br)
         {
             ulong value = 0;
