c11926fddbd0092fe9b75b286cfd5bcafaa02f31
UbbeLoL <netdeob@gmail.com>
2013-02-13 19:15:11 +0100

    Updated reserved packet IDs

--- uNet/Globals.cs
+++ uNet/Globals.cs
@@ -11,6 +11,6 @@ namespace uNet
     internal static class Globals
     {
         public static string Version = "0.0.1";
-        public static List<short> ReservedPacketIDs = new List<short> { 0, 1 };
+        public static List<short> ReservedPacketIDs = new List<short> { 9998, 9999 };
     }
 }
